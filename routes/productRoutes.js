b490c875481016e2f83e0e6a8dd1e18c62fc91a1
Ajaybanu <ajaybanu7@gmail.com>
2023-10-06 22:49:33 +0530

    products filter

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,20 @@
 import express from "express";
 import {
  
+ 
   createProductController,
   deleteProductController,
   getProductController,
   getSingleProductController,
-
+  productCategoryController,
+  productCountController,
+  productFiltersController,
+  productListController,
   productPhotoController,
+  realtedProductController,
+  searchProductController,
   updateProductController,
+
 } from "../controllers/productController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import formidable from "express-formidable";
@@ -43,6 +50,23 @@ router.get("/product-photo/:pid", productPhotoController);
 //delete rproduct
 router.delete("/delete-product/:pid", deleteProductController);
 
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+//search product
+router.get("/search/:keyword", searchProductController);
+
+//similar product
+router.get("/related-product/:pid/:cid", realtedProductController);
+
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
 
 
 
